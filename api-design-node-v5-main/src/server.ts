2411ae43e7ca0aacdd1f6c7c822ce5fb62979bae
sakib <alifsakib1@gmail.com>
2026-06-29 00:13:25 +0600

    fix: update health check response and organize route imports

--- api-design-node-v5-main/src/server.ts
+++ api-design-node-v5-main/src/server.ts
@@ -1,14 +1,19 @@
- import express from 'express';
+import express from 'express';
+import habitRoutes from './routes/habitRoutes.ts';
+import userRoutes from './routes/userRoutes.ts';
+import authRoutes from './routes/authRoutes.ts';
+
 
  const app = express();
 
  app.get('/health', (req, res)=> {
-
-    res.send(
-        "<button> click me </button>"
-    )
-    res.json({ message: 'Server is healthy and HELLO' }).
-    status(200);
+    res.send('Server is healthy and running');
  })
+
+
+app.use('/api/auth', authRoutes);
+app.use('/api/habits', habitRoutes);
+app.use('/api/users', userRoutes);
+
 
  export default app;
