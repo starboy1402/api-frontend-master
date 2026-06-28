c04a7472229ffce75cef70ebb0043446ecf17fc2
sakib <alifsakib1@gmail.com>
2026-06-28 20:51:28 +0600

    refactor: use environment variable for server port configuration

--- api-design-node-v5-main/src/index.ts
+++ api-design-node-v5-main/src/index.ts
@@ -4,6 +4,8 @@ application
 
 import app from "./server.ts";
 
-app.listen( 3000 , () => {
-    console.log('Server is running on port 3000');
+const PORT = Number(process.env.PORT ?? 3000);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
